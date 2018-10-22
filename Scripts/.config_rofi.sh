07beecd27cdb6481cb634f561c5c16d9ce74c5de
zami3l <null@zami3l.com>
2018-10-22 21:07:29 +0200

    Ajout d'un dossier script, script service vmware et script run config rofi

---
+++ Scripts/.config_rofi.sh
@@ -0,0 +1,5 @@
+# Raccourci situé dans /etc/profile.d/
+
+# Script permettant de charger la config couleur de Rofi
+xrdb /home/zamiel/Documents/Scripts/.Xressources
+
---
+++ Scripts/.vmware_service.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+systemctl start vmware-hostd.service vmware-networks.service vmware-usbarbitrator.service
