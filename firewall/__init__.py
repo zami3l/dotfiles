86ceecf00f97309b5a7966afbf74c43f74dfe1b5
zami3l <null@zami3l.com>
2019-02-06 21:20:07 +0100

    init(*): ajout fichiers

---
+++ firewall/__init__.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8 -*-
+
+__version__ = "1.0"
+__author__ = "Zami3l"
+
+from fonction import iptables
---
+++ firewall/firewall.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8 -*-
+
+import os
+
+from fonction import iptables
+
