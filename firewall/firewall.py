1178f1197d53fdb32916318795dacaedc93e8864
zami3l <null@zami3l.com>
2019-02-07 23:48:26 +0100

    refactor(*): ajout initialisation et politiques

--- firewall/firewall.py
+++ firewall/firewall.py
@@ -1,6 +1,22 @@
 # -*-coding:utf-8 -*-
 
 import os
+from fonction.iptables import Iptables
 
-from fonction import iptables
+#Déclaration des interfaces présentes
+Interface_Local = 'lo'
+Interface_LAN = 'LAN'
+Interface_WAN = 'WAN'
+Interface_DMZ = 'DMZ'
+Interface_VM = 'VM'
 
+#Affectation de la classe Iptables
+Cmd_Iptables = Iptables()
+
+#On initialise iptables
+Cmd_Iptables.initialisation()
+
+#On change les politiques par défaut
+Cmd_Iptables.commande('filter', 'INPUT', 'DROP', 'Politique_Simple')
+Cmd_Iptables.commande('filter', 'FORWARD', 'DROP', 'Politique_Simple')
+Cmd_Iptables.commande('filter', 'OUTPUT', 'ACCEPT', 'Politique_Simple')
