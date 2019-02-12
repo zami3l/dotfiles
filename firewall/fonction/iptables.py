2deef01ab278a260655843ef6f315f12ee760dec
zami3l <null@zami3l.com>
2019-02-12 20:38:55 +0100

    style(fonction): ajout des commentaires pour les fonctions test argument et commande globale

--- firewall/fonction/iptables.py
+++ firewall/fonction/iptables.py
@@ -12,9 +12,11 @@ class Iptables:
     Liste['Chaine'] = 'INPUT', 'FORWARD', 'PREROUTING', 'POSTROUTING', 'OUTPUT'
     Liste['Action'] = 'ACCEPT', 'DENY', 'REJECT', 'DROP'
 
+    #Déclaration des paramètres acceptés par la fonction Commande Globale
     Parametres_Commande = {'In_Interface': None, 'Out_Interface': None, 'Protocole': None, 'Destination': None,
                     'Destination_Protocole': None, 'Filter': None, 'Action': None, 'Table': None}
 
+    #Declaration d'une liste pour la sauvegarde des paramètres trouvés
     Save_Parametres = [0] * len(Parametres_Commande.keys())
 
     #Fonction Initialisation : Permet d'initialiser iptables
@@ -43,62 +45,95 @@ class Iptables:
 
         #Paramétrage des politiques générales
         if Mode == 'Politique_Simple':
+
             result = "iptables -P {} {}".format(Chaine, Action)
 
         #Paramétrage des politiques d'une table
         elif Mode == 'Politique_Avancee':
+
             result = "iptables -t {} -P {} {}".format(Table, Chaine, Action)
 
         #Reset des compteurs d'une table
         elif Mode == 'Reset':
+
             result = "iptables -t {} -Z".format(Table)
         
         #Supprimer toutes les règles actives d'une table
         elif Mode == 'Del_Regle':
+
             result = "iptables -t {} -F".format(Table)
 
         #Supprimer toutes les chaines personnalisées d'une table
         elif Mode == 'Del_Chaine':
+
             result = "iptables -t {} -X".format(Table)
 
         print(result)
 
         return result
 
+    #Fonction Test Argument : Permet de vérifier et de sauvegarder les paramètres passés dans la fonction Commande Globale
     def Test_Argument(self, Argument):
 
         for Index in range(0, len(self.Parametres_Commande.keys())):
 
             if Argument == list(self.Parametres_Commande.keys())[Index]:
+                
+                #Si un argument est trouvé dans la liste, on sauvegarde le paramètre et on sort de la boucle FOR
                 self.Save_Parametres[Index] = True
                 break
 
+    # Fonction Commande Globale : Permet d'ajouter des règles dans le Firewall
+    def commande_globale(self, **Parametres):
 
-    def commande_global(self, **Parametres):
-
+        #On reset la liste de sauvegarde des paramètres
         self.Save_Parametres = [0] * len(self.Parametres_Commande.keys())
 
         result = "iptables "
 
+        # On lance la fonction Test Argument
         for Index in range(0, len(Parametres.keys())):
+
             self.Test_Argument(list(Parametres.keys())[Index])
 
-
+        # On affecte le paramètre In Interface
         if self.Save_Parametres[0] == True:
+
             result += "-i {} ".format(Parametres.get('In_Interface'))
+
+        # On affecte le paramètre Out Interface
         if self.Save_Parametres[1] == True:
+
             result += "-o {} ".format(Parametres.get('Out_Interface'))
+
+        # On affecte le paramètre Protocole
         if self.Save_Parametres[2] == True:
+
             result += "-p {} ".format(Parametres.get('Protocole'))
+
+        # On affecte le paramètre Destination
         if self.Save_Parametres[3] == True:
+
             result += "-d {} ".format(Parametres.get('Destination'))
+
+        # On affecte le paramètre Destionation Protocole
         if self.Save_Parametres[4] == True:
+
             result += "-dport {} ".format(Parametres.get('Destination_Protocole'))
+
+        # On affecte le paramètre Filter
         if self.Save_Parametres[5] == True:
+
             result += "-A {} ".format(Parametres.get('Filter'))
+
+        # On affecte le paramètre Action
         if self.Save_Parametres[6] == True:
+
             result += "-j {} ".format(Parametres.get('Action'))
+
+        # On affecte le paramètre Table
         if self.Save_Parametres[7] == True:
+
             result += "-n {} ".format(Parametres.get('Table'))
 
         print(result)
